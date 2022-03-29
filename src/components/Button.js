8b782ce31c1e769f2cb8c0130e8e6c517751d3c3
Mariantonieta <mariantonietachacondibacco@gmail.com>
2022-03-29 16:49:16 +0200

    first-commit

---
+++ src/components/Button.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../styles/button.css';
+
+const Button = (props) => {
+  let classes = 'button ';
+  classes += props.operation ? 'operation' : '';
+  classes += props.double ? 'double' : '';
+  classes += props.triple ? 'triple' : '';
+
+  return (
+    // <button
+    //   className={`
+    //   button
+    //   ${props.operation ? 'operation' : ''}
+    //   ${props.double ? 'double' : ''}
+    //   ${props.triple ? 'triple' : ''}
+    // `}
+    // >
+    //   {props.label}
+    // </button>
+
+    <button
+      onClick={e => props.click && props.click(props.label)}
+      className={classes}
+    >
+      {props.label}
+    </button>
+  );
+};
+export default Button;
---
+++ src/components/Display.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../styles/display.css'
+const Display = (props) => {
+    return (
+      <div className="display">{props.value}</div>
+    )
+}
+
+export default Display
